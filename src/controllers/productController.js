df9d7f136e45f4dbc2fba589bbd88bf8df503c4e
chucky-golden <chukschibyke92@gmail.com>
2023-12-19 16:51:46 +0100

    changes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -132,9 +132,9 @@ const fetchProducts = async (req, res) => {
         let uid = req.params.id
 
         let productids = await ProductImport.find({ vendorId: uid }, { projection: { _id: 0, productid: 1 } })
-        if (productids !== null) {
+        if (productids !== null && productids.length > 0) {
 
-            let response = await axios.get('https://admin-dqcw.onrender.com/vendors/getvendorproducts', {
+            let response = await axios.post('https://admin-dqcw.onrender.com/vendors/getvendorproducts', {
                 products: productids
             })
 
@@ -146,7 +146,7 @@ const fetchProducts = async (req, res) => {
 
         }
         else {
-            res.json({ message: 'error handling request' })
+            res.json({ message: 'No products found for the given vendor ID' });
         }
         
 
