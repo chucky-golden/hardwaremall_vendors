671abf99706677fc854753502583936871200973
chucky-golden <chukschibyke92@gmail.com>
2023-12-20 20:22:30 +0100

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,8 +16,9 @@ const getProducts = async (req, res) => {
             for(let x = 0; x < importedProducts.length; x++){
                 if(data._id === importedProducts[x].productid){
                     let details = []
+
                     for(let i = 0; i < vendors.length; i++){
-                        if(vendors[i]._id === importedProducts[i].vendorId){
+                        if(vendors[i]._id === importedProducts[x].vendorId){
                             details.push(vendors[i])
                         }
                     }
@@ -55,8 +56,9 @@ const getTopProducts = async (req, res) => {
             for(let x = 0; x < importedProducts.length; x++){
                 if(data._id === importedProducts[x].productid){
                     let details = []
+
                     for(let i = 0; i < vendors.length; i++){
-                        if(vendors[i]._id === importedProducts[i].vendorId){
+                        if(vendors[i]._id === importedProducts[x].vendorId){
                             details.push(vendors[i])
                         }
                     }
