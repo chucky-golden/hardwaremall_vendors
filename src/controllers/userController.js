8a0f21c701a5f8ff253c3b7949af676577f0dd0d
chucky-golden <chukschibyke92@gmail.com>
2023-12-26 19:36:52 +0100

    changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,11 +55,15 @@ const getTopProducts = async (req, res) => {
         products.forEach(data => {
             let details = []
             for(let x = 0; x < importedProducts.length; x++){
-                if(data._id == importedProducts[x].productid){
+                if(data._id === importedProducts[x].productid){
+
+                    console.log('match found p')
 
                     for(let i = 0; i < vendors.length; i++){
-                        if(vendors[i]._id == importedProducts[x].vendorId){
+                        if(vendors[i]._id === importedProducts[x].vendorId){
                             details.push(vendors[i])
+
+                            console.log('match found v')
                         }
                     }
                 }
